8ae2875b26ae72bdb7405dfeb1a4ad1b7737dc6b
Mauricio <mauricio.guzman.salazar@gmail.com>
2017-11-17 22:36:55 +0000

    creates relation between products and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable
   include DeviseTokenAuth::Concerns::User
+  
+  has_many :products, dependent: :destroy
 end
