d61eeb8b4c5c032bcd69928f58c7cde6a9f95a7a
Mauricio <mauricio.guzman.salazar@gmail.com>
2017-11-18 23:31:50 +0000

    changes id to user_id in products controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ProductsController < ApplicationController
   before_action :authenticate_user!, except: [ :index ]
   
     def index
-      @user = User.find(params[:id])
+      @user = User.find(params[:user_id])
       @products = @user.products
       render json: @products
     end
