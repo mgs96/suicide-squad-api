b5f9eb82cc9dbcc9df09d494f08d21aee74be4e7
Mauricio <mauricio.guzman.salazar@gmail.com>
2017-11-21 15:42:35 +0000

    deleting a product will delete its comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   has_many :tags, dependent: :destroy
   has_many :categories, through: :tags
   
-  has_many :comments
+  has_many :comments, dependent: :destroy
   
   def self.by_category(category = "")
     self.joins(:categories).where('categories.name': category)
