cc66bc0703dd7dd1e61b715623a5e1db3fac699a
Mauricio <mauricio.guzman.salazar@gmail.com>
2017-11-21 03:18:24 +0000

    adds Comment model, references products with comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,10 @@
 class Product < ApplicationRecord
   belongs_to :user
   
-  has_many :tags
+  has_many :tags, dependent: :destroy
   has_many :categories, through: :tags
+  
+  has_many :comments
   
   def self.by_category(category = "")
     self.joins(:categories).where('categories.name': category)
